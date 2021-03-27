26eaf0e0b4eb30ebeb3d296dfb8513873816afa4
Miguel Caraballo de la Rosa <mcaraballodelarosa@outlook.com>
2021-03-27 17:48:28 +0100

    Revert "Add project configuration #1"
    This reverts commit 21bff1cdeeb279f7c02492bf19ca849772dcd568.

--- packages/domain/lib/base_usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:data/failure.dart';
-
-abstract class BaseUseCase<T, Params> {
-  Future<Either<Failure, T>> call(Params params);
-}
-
-class NoParams {}
